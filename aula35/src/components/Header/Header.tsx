678d850b10772415430c1724568636208683c892
gi <85405443+giovannarevitoroz@users.noreply.github.com>
2024-08-09 11:47:35 -0300

    Add files via upload

---
+++ aula35/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import './header.css'
+
+
+function Header() {
+    return (
+        <>
+            <header>
+                <h1>Cabeçalho</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header;
