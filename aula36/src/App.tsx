13b2f69bf4844225f482417015e80931d13ed053
gi <85405443+giovannarevitoroz@users.noreply.github.com>
2024-08-15 09:55:24 -0300

    Add files via upload

---
+++ aula36/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header"
+
+export default function App(){
+  return(
+    <>
+    <Header/>
+    <div>Hello World</div>
+    <div>Hello World</div>
+    <p className = "loremApp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ipsum in dolore quam ipsam inventore, deserunt aperiam, explicabo quaerat accusantium reprehenderit ducimus! Explicabo velit commodi, ab dolore nihil assumenda deserunt.</p>
+    </>
+  )
+}
---
+++ aula36/src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+
+import './Header.css'
+
+export default function Header(){
+
+    return(
+        <>
+            <header>
+                <h1>Cabeçalho</h1>
+                <nav>
+                    <ul>
+                        <li>Inicio</li>
+                        <li>Comprar</li>
+                        <li>Ajuda</li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
